72651725379477daa4e66a41b7dff7f2c70ce857
Liu Zhongyu <lzy19980525@yeah.net>
2019-05-23 16:51:24 +0800

    添加 ChartActivity 界面并更新相关资源

---
+++ app/src/main/java/com/huojitang/leaf/ChartActivity.kt
@@ -0,0 +1,12 @@
+package com.huojitang.leaf
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart)
+    }
+}
