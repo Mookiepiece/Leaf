4d02183a73c9ed98b6de565a3f78eae469e6afb5
DeweyXuan <deweyxuan@gmail.com>
2019-05-25 23:38:09 +0800

    主界面和心愿单界面的再次修改

--- app/src/main/java/com/huojitang/leaf/WishFragment.java
+++ app/src/main/java/com/huojitang/leaf/WishFragment.java
@@ -1,25 +1,38 @@
 package com.huojitang.leaf;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WishFragment<FragmentAdapter> extends Fragment {
     private String[] names = {"相机","小米九","平板电脑","内存条","移动硬盘"};
     private String[] price = {"2000.00元","3199.00元","1700.00元","300.00元","500.00元"};
     private String[] states = {"已完成","已完成","已取消","未完成","已取消"};
-    private String[] starttime = {"2019-1","2019-1","2019-2","2019-4","2019-4"};
-    private String[] endtime = {"2019-3","2019-4","2019-3","","2019-5"};
-    ListView listView;
+    private String[] starttime = {"2019-01","2019-01","2019-02","2019-04","2019-04"};
+    private String[] endtime = {"2019-03","2019-04","2019-03","","2019-05"};
+
+    private List<WishMessage> wishMessageList = new ArrayList<>();
+    SwipeMenuListView mListView;
+
 
     @Nullable
     @Override
@@ -28,47 +41,110 @@ public class WishFragment<FragmentAdapter> extends Fragment {
                              @Nullable Bundle savedTnstanceState){
         View view = inflater.inflate(R.layout.fragment_wish,container,false);
 
-        listView = (ListView) view.findViewById(R.id.LV_2);
+        mListView = (SwipeMenuListView) view.findViewById(R.id.LV_2);
+        initWishMessage();
 
-        WishFragment.FragmentAdapter fragmentAdapter = new WishFragment.FragmentAdapter();
-        listView.setAdapter(fragmentAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final WishFragment.FragmentAdapter fragmentAdapter = new WishFragment.FragmentAdapter();
+        mListView.setAdapter(fragmentAdapter);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                AlertDialog.Builder builder;
-                builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("删除？").setNegativeButton(R.string.g_back,null);
-                builder.setPositiveButton(R.string.g_confirm, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        getActivity().notifyAll();
-                    }
-                });
-                builder.show();
+            public void create(SwipeMenu menu) {
+                SwipeMenuItem finishItem = new SwipeMenuItem(getContext());
+                finishItem.setBackground(new ColorDrawable(Color.rgb(0xc9,0xc9,0xce)));
+                finishItem.setWidth(dp2px(90));
+                finishItem.setTitle("完成");
+                finishItem.setTitleSize(18);
+                finishItem.setTitleColor(Color.WHITE);
+
+                menu.addMenuItem(finishItem);
+
+                SwipeMenuItem cancelItem = new SwipeMenuItem(getContext());
+                cancelItem.setBackground(new ColorDrawable(Color.rgb(0xd4,0xd4,0xdc)));
+                cancelItem.setWidth(dp2px(90));
+                cancelItem.setTitle("取消");
+                cancelItem.setTitleSize(18);
+                cancelItem.setTitleColor(Color.WHITE);
+
+                menu.addMenuItem(cancelItem);
+
+                SwipeMenuItem addItem = new SwipeMenuItem(getContext());
+                addItem.setBackground(new ColorDrawable(Color.rgb(0xf9,0x3f,0x25)));
+                addItem.setWidth(dp2px(90));
+                addItem.setTitle("添加");
+                addItem.setTitleSize(18);
+                addItem.setTitleColor(Color.WHITE);
+
+                menu.addMenuItem(addItem);
             }
-        });
+        };
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        mListView.setMenuCreator(creator);
+
+        mListView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                AlertDialog.Builder builder;
-                builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("添加？").setNegativeButton(R.string.g_back,null);
-                builder.setPositiveButton(R.string.g_confirm, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        ;
-                    }
-                });
-                builder.show();
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                WishMessage message = wishMessageList.get(position);
+                switch (index){
+                    case 0:
+                        Toast.makeText(getContext(),"you clicked finish",Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getContext(),"you clicked cancel",Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getContext(),"you clicked add",Toast.LENGTH_SHORT).show();
+                        break;
+                }
                 return false;
             }
         });
+        mListView.setOnSwipeListener(new SwipeMenuListView.OnSwipeListener() {
+            @Override
+            public void onSwipeStart(int position) {
+
+            }
+
+            @Override
+            public void onSwipeEnd(int position) {
+
+            }
+        });
+
+        mListView.setOnMenuStateChangeListener(new SwipeMenuListView.OnMenuStateChangeListener() {
+            @Override
+            public void onMenuOpen(int position) {
+
+            }
+
+            @Override
+            public void onMenuClose(int position) {
+
+            }
+        });
+
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(),position+"long click",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        setHasOptionsMenu(true);
         return view;
     }
 
-    public class FragmentAdapter extends BaseAdapter{
+    private void initWishMessage() {
+        for(int i = 0;i<names.length;i++){
+            WishMessage wishMessage = new WishMessage(names[i],price[i],states[i],starttime[i],endtime[i]);
+            wishMessageList.add(wishMessage);
+        }
+    }
+
+    public class FragmentAdapter extends BaseSwipListAdapter{
         @Override
         public int getCount() {
-            return names.length;
+            return wishMessageList.size();
         }
 
         @Override
@@ -84,20 +160,47 @@ public class WishFragment<FragmentAdapter> extends Fragment {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View view = getLayoutInflater().inflate(R.layout.wish_list,null);
+            new ViewHolder(view);
 
-            TextView textView1 = view.findViewById(R.id.wish_name);
-            TextView textView2 = view.findViewById(R.id.wish_price);
-            TextView textView3 = view.findViewById(R.id.wish_state);
-            TextView textView4 = view.findViewById(R.id.wish_start_time);
-            TextView textView5 = view.findViewById(R.id.wish_end_time);
+            ViewHolder holder = (ViewHolder) view.getTag();
 
-            textView1.setText(names[position]);
-            textView2.setText(price[position]);
-            textView3.setText(states[position]);
-            textView4.setText(starttime[position]);
-            textView5.setText(endtime[position]);
+            holder.wishName.setText(wishMessageList.get(position).getWishName());
+            holder.wishPrice.setText(wishMessageList.get(position).getWishPrice());
+            holder.wishState.setText(wishMessageList.get(position).getWishState());
+            holder.startTime.setText(wishMessageList.get(position).getStartTime());
+            holder.endTime.setText(wishMessageList.get(position).getEndTime());
             return view;
         }
+
+        class ViewHolder{
+            TextView wishName;
+            TextView wishPrice;
+            TextView wishState;
+            TextView startTime;
+            TextView endTime;
+
+            public ViewHolder(View view){
+                wishName = view.findViewById(R.id.wish_name);
+                wishPrice = view.findViewById(R.id.wish_price);
+                wishState = view.findViewById(R.id.wish_state);
+                startTime = view.findViewById(R.id.wish_start_time);
+                endTime = view.findViewById(R.id.wish_end_time);
+
+                view.setTag(this);
+            }
+        }
+    }
+    private int dp2px(int value){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,value,getResources().getDisplayMetrics());
+    }
+    private int dp2px(float value){
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int)(value*scale+0.5f);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
 
