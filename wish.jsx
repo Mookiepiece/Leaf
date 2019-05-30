da44a58c59179150744534473a1cb78826499938
Mookiepiece <1779834460@qq.com>
2019-05-30 19:17:56 +0800

    emm

--- wish.jsx
+++ wish.jsx
@@ -8,39 +8,100 @@ var ViewLength;
 const javaGetValueEvent = new CustomEvent("javaGetValue");
 
 //从java获取起始与结束的月份
-var BeginMonth = { year: 2019, month: 10 }, EndMonth = { year: 2023, month: 8 };
-//JAVA暴露的对象
+var FirstWishStartTime, LastWishEndTime;
 
-var java;
-
+//模拟JAVA暴露的对象
+var java={
+    getBeginAndEndMonth:()=>{
+        javaDataReceiver.getBeginAndEndMonth('{"FirstWishStartTime":{"year":2019,"month":10},"LastWishEndTime":{"year":2023,"month":8}}');
+    },
+    javaGetMonthVal:(json)=>{
+        let jj=JSON.parse(json);
+        let r=[];
+        for(let i=0;i<jj.length;i++){
+            let a={"date":{"year":2021,"month":12},"item":[{"name":"item1","state":2},{"name":"item2","state":1},{"name":"item2","state":1},{"name":"item2","state":2},{"name":"item2","state":0}]}
+            let d=jj;
+            for(let j=0;j<i;j++)
+                d=preMonth(d);
+            a.date=d;
+            r.unshift(a);
+        }
+        javaDataReceiver.javaGetMonthVal(JSON.stringify(r));
+    },
+    loadMonth:(json)=>{
+        let jj=JSON.parse(json);
+        let r={"date":{"year":jj.year,"month":jj.month},"item":[{"name":"item1","state":2},{"name":"item dad dettr ex2","state":1},{"name":"item2 trwt","state":1},{"name":"item2","state":2},{"name":"item2","state":0}]}
+        r=[r];
+        javaDataReceiver.javaGetMonthVal(JSON.stringify(r));
+    }
+};
 
 
 //通往java的钥匙
-var javaDataLoader = (() => {
+var javaDataReceiver = (() => {
     let V = {};
     V.lock = false;
 
-    V.getBeginAndEndMonth=()=>{
-        
+    V.getBeginAndEndMonth = (str) => {
+         let json= JSON.parse(str);
+         FirstWishStartTime=json.FirstWishStartTime;
+         LastWishEndTime=json.LastWishEndTime;
     }
 
     //TODO 从java获取值
     V.javaGetMonthVal = (str) => {
-
-        V.javaReturnVal = JSON.parse(str)
-
-
-            // V.javaReturnVal =  getInitialVal();
-        
-        
+        V.javaReturnVal = JSON.parse(str);
         window.dispatchEvent(javaGetValueEvent);
     }
     return V;
 })();
 
+window.onload = () => {
+    calculateVLength();
+
+    //第一步获取最开始和最晚的月份
+    let a;
+    java.getBeginAndEndMonth();
+    a = setInterval(()=>{
+        if(FirstWishStartTime!=null){
+            window.clearInterval(a);
+            initData();
+        }
+    },50)
+
+    /** 根据页面高度动态计算初始显示的月份数
+     *  CSS设置的每月最短是150px
+     *  默认最小是6
+     */
+    function calculateVLength() {
+        let t = Math.ceil(window.innerHeight / 150.0);
+        ViewLength = t > 6 ? t : 6;
+        console.log(ViewLength);
+    }
+}
+
+
+//加载很多很多个月份的数据
+function initData(){
+    if(javaDataReceiver.lock)
+        return;
+    javaDataReceiver.lock=true;
+    let listener;
+    new Promise((resolve, reject) => {
+        listener = () => { resolve() };
+        window.addEventListener("javaGetValue", listener);
+        java.javaGetMonthVal(JSON.stringify({ year: 2021, month: 12,length:ViewLength}));
+    }).then(() => {
+        window.removeEventListener("javaGetValue", listener);
+        ReactDOM.render(<Root />, root);
+        javaDataReceiver.lock=false;
+    })
+}
+
+
 //判断某月是否有记录
-function hasMonthVal(date) {
-    if (ge(BeginMonth, date) || ge(date, EndMonth))
+function avalialeMonth(date) {
+    if (ge(FirstWishStartTime, date) || ge(date, LastWishEndTime))
         return false;
     return true;
 
@@ -59,15 +120,26 @@ class Root extends React.Component {
 
     constructor() {
         super();
-        this.V = javaDataLoader.javaReturnVal;
+        this.V = javaDataReceiver.javaReturnVal;
         this.beginDiv = React.createRef();
         this.endDiv = React.createRef();
-
-        this.state = {
-            monthes: this.V,
-            beginDivOff: false,
-            endDivOff: false,
+        this.beforeScroll={};
+        
+        if(this.V.length<ViewLength){
+            this.state = {
+                monthes: this.V,
+                beginDivOff: true,
+                endDivOff: true,
+            }
         }
+        else{
+            this.state = {
+                monthes: this.V,
+                beginDivOff: false,
+                endDivOff: false,
+            }
+        }
+
 
         this.i = 0;
     }
@@ -80,71 +152,67 @@ class Root extends React.Component {
     }
 
     scrollHandler() {
-        console.log(this.root.scrollTop);
 
-        if (javaDataLoader.lock == true)
+        //上锁
+        if (javaDataReceiver.lock == true)
             return;
-        javaDataLoader.lock = true;
-        //TODO 是否使用React ref替换DOM和魔法值
-        if (this.beginDiv.current && this.insight({ offsetTop: 0, clientHeight: 150 })) { //名为startdiv的块处于视野 加载上月值
-            let targetMonth = preMonth(this.getBeginDate());//begin
-            if (hasMonthVal(targetMonth)) {
-                this.beforeScroll = { direction: "begin" };//begin
+        javaDataReceiver.lock = true;
+
+        //判断视口滚动到头,以加载上/下个月的数据
+        let flag = 0;
+        if (this.beginDiv.current && this.insight({ offsetTop: 0, clientHeight: 150 })) flag = 1;
+        if (this.endDiv.current && this.insight({ offsetTop: this.root.scrollHeight - 150, clientHeight: 150 })) flag = 2;
+        if (flag != 0) {
+            let targetMonth;  //上/下个月
+            targetMonth = (flag == 1 ? preMonth(this.getDataBeginDate()) : nextMonth(this.getDataEndDate()));
+            if (avalialeMonth(targetMonth)) { //判断上/下个月是否到头 到头就触发不能滚动
+                flag == 1 ? this.beforeScroll = { direction: "begin" } : this.beforeScroll = { direction: "end" };
                 
+                //异步操作:读取JAVA数据
                 new Promise((resolve, reject) => {
                     this.listener = () => { resolve() };
                     window.addEventListener("javaGetValue", this.listener);//监听java返回值
-                    javaDataLoader.javaGetMonthVal(targetMonth);//call java
+                    java.loadMonth(JSON.stringify(targetMonth));//call java
                 }).then(() => {
                     window.removeEventListener("javaGetValue", this.listener);
-                    this.setState((state) => ({
-                        monthes: [...javaDataLoader.javaReturnVal, ...state.monthes],//begin
-                    }));
+                    if (flag == 1)
+                        this.setState((state) => ({
+                            monthes: [...javaDataReceiver.javaReturnVal, ...state.monthes],
+                        }))
+                    else
+                        this.setState((state) => ({
+                            monthes: [...state.monthes, ...javaDataReceiver.javaReturnVal],
+                        }));
                 });
             }
             else {
-                this.setState(() => ({ beginDivOff: true }));
+                if (flag == 1)
+                    this.setState(() => ({ beginDivOff: true }));
+                else
+                    this.setState(() => ({ endDivOff: true }));
             }
             return;
         }
-        if (this.endDiv.current && this.insight({ offsetTop: this.root.scrollHeight - 150, clientHeight: 150 })) { //名为enddiv的块处于视野 加载下月值
-            let targetMonth = nextMonth(this.getEndDate());//end
-            if (hasMonthVal(targetMonth)) {
-                this.beforeScroll = { direction: "end" };//end
 
-                new Promise((resolve, reject) => {
-                    this.listener = () => { resolve() };
-                    window.addEventListener("javaGetValue", this.listener);
-                    javaDataLoader.javaGetMonthVal(targetMonth);
-                }).then(() => {
-                    window.removeEventListener("javaGetValue", this.listener);
-                    this.setState((state) => ({
-                        monthes: [...state.monthes, ...javaDataLoader.javaReturnVal],//end
-                    }));
-                });
-            }
-            else {
-                this.setState(() => ({ endDivOff: true }));
-            }
-            return;
-        }
-        javaDataLoader.lock = false;
+        javaDataReceiver.lock = false;
     }
 
-    getBeginDate = () => {
+    //遍历数组找最早月
+    getDataBeginDate = () => {
         if (this.state.monthes.length == 0)
             return null;
         return this.state.monthes[0].date;
     }
 
-    getEndDate = () => {
+    //遍历数组找最晚月
+    getDataEndDate = () => {
         if (this.state.monthes.length == 0)
             return null;
         return this.state.monthes[this.state.monthes.length - 1].date;
     }
 
     getSnapshotBeforeUpdate() {
-        if (javaDataLoader.lock == false)
+        if (javaDataReceiver.lock == false)
             return null;
         return { scrollHeight: this.root.scrollHeight, scrollTop: this.root.scrollTop }
     }
@@ -152,15 +220,13 @@ class Root extends React.Component {
     componentDidUpdate(preProps, preState, snapshot) {
         this.i++;
 
-        if (javaDataLoader.lock == false)
+        if (javaDataReceiver.lock == false)
             return;
         //重置视口位置以防止新加载的数据把旧数据挤出视口
         if (this.beforeScroll.direction == "begin") {
             this.root.scrollTop = snapshot.scrollTop - snapshot.scrollHeight + this.root.scrollHeight;
-            this.savedScrollTop = this.root.scrollTop;
-            console.log("did   " + this.root.scrollTop);
         }
-        javaDataLoader.lock = false; //获取数据完成，解除锁2
+        javaDataReceiver.lock = false; //获取数据完成，解除锁
     }
 
     render() {
@@ -168,7 +234,7 @@ class Root extends React.Component {
         let endDiv = this.state.endDivOff ? null : <div id="endDiv" ref={this.endDiv}></div>;
 
         return (
-            <div id="sub-root" >
+            <div id="sub-root">
                 {beginDiv}
                 {
                     this.state.monthes.map((val, idx) => {
@@ -204,38 +270,29 @@ class Month extends React.Component {
 
 class ItemDiv extends React.Component {
     render() {
-        let className = this.props.state;
+        let color;
+        switch(this.props.state){
+            case 0:
+                color=" mdui-color-blue mdui-text-color-white-text"; break;
+            case 1:
+                color=" mdui-color-pink-50 mdui-text-color-black-secondary"; break;
+            case 2:
+                color=" mdui-color-green-300 mdui-text-color-white-text"; break;
+        }
 
         return (
-            <div className={"item " + className} onClick={() => { alert(this.props.name) }}>
+            <button className={"mdui-btn mdui-ripple item " + color} onClick={() => { alert(this.props.name) }} >
                 {this.props.name + " " + this.props.state}
-            </div>
+            </button>
+            
+        /*<div className={"item " + className} }>
+                        
+        </div>*/
         );
     }
 }
 
-window.onload = () => {
-    let listener;
-    new Promise((resolve, reject) => {
-        listener = () => { resolve() };
-        window.addEventListener("javaGetValue", listener);
-        javaDataLoader.javaGetMonthVal({year:2021,month:12},6);
-    }).then(() => {
-        window.removeEventListener("javaGetValue", listener);
-        ReactDOM.render(<Root />, root);
-    })
 
-}
-
-/** 根据页面高度动态计算应该显示的月份数
- *  CSS设置的每月最短是150px
- *  默认最小是6 （的2倍）
- */
-function calculateVLength() {
-    let t = Math.ceil(window.innerHeight / 150.0);
-    ViewLength = t > 6 ? t : 6;
-    console.log(ViewLength);
-}
 
 function preMonth(ym) {
     let { year, month } = ym;
@@ -253,60 +310,3 @@ function nextMonth(ym) {
         return { year: year, month: month + 1 };
 }
 
-
-// /**
-//  * TEST
-//  */
-// function getInitialVal() {
-//     return [
-//         {
-//             date: { year: 2021, month: 12 },
-//             item: [
-//                 { name: "item1", state: "resolved" },
-//                 { name: "item2", state: "cancelled" },
-//                 { name: "item2", state: "cancelled" },
-//                 { name: "item2", state: "resolved" },
-//                 { name: "item2", state: "onprogress" },
-//             ]
-//         },
-//         {
-//             date: { year: 2022, month: 1 },
-//             item: [
-//                 { name: "item1", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//             ]
-//         },
-//         {
-//             date: { year: 2022, month: 2 },
-//             item: [
-//                 { name: "item1", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//             ]
-//         },
-//         {
-//             date: { year: 2022, month: 3 },
-//             item: [
-//                 { name: "item1", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//             ]
-//         },
-//         {
-//             date: { year: 2022, month: 4 },
-//             item: [
-//                 { name: "item1", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//             ]
-//         },
-//         {
-//             date: { year: 2022, month: 5 },
-//             item: [
-//                 { name: "item1", state: "resolved" },
-//                 { name: "item2", state: "resolved" },
-//             ]
-//         }
-//     ];
-// }
