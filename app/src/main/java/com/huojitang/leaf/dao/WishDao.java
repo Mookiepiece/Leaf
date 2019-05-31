6595f1c2a68ecb8807735f12df6617b51161f160
Liu Zhongyu <lzy19980525@yeah.net>
2019-05-31 22:42:34 +0800

    完善一些功能 1. 当应用启动时，如果当前月的账单（monthlyBill）不存在，则会新建一个，并添加到数据库 2. 针对 WishDao 提供用于获取心愿的最早开始时间和最晚结束时间的 API 3. 针对 Tag 的 color 属性的变化，修改图表代码以适应变化

--- app/src/main/java/com/huojitang/leaf/dao/WishDao.java
+++ app/src/main/java/com/huojitang/leaf/dao/WishDao.java
@@ -50,6 +50,22 @@ public class WishDao extends BaseDao<Wish> {
         return LitePal.findAll(Wish.class);
     }
 
+    /**
+     * 返回所有心愿里的最早开始时间
+     * @return 一个字符串（yyyy-MM），表示心愿单中所有心愿的最早开始时间
+     */
+    public String getEarliestStartTime() {
+        return LitePal.min(Wish.class, "startTime", String.class);
+    }
+
+    /**
+     * 返回所有心愿里的最晚结束时间
+     * @return 一个字符串（yyyy-MM）表示心愿单中所有心愿的最晚结束时间
+     */
+    public String getLatestFinishedTime() {
+        return LitePal.max(Wish.class, "finishedTime", String.class);
+    }
+
     private static WishDao instance = new WishDao();
 
     private WishDao() {}
